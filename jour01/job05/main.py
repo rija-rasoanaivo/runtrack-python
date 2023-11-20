ce30a8a34646e2c12c860353184fb7dec918945d
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-20 11:57:15 +0100

    ajout du job05

---
+++ jour01/job05/main.py
@@ -0,0 +1,4 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_inverse = alphabet[::-1]  # Inverser l'alphabet, []créé une liste qui est mutable, ce qui signifie que vous pouvez modifier son contenu. : correspond au debut de la sequence (a), : le second correspond a la fin de la sequence donc (z) et -1 permet de le faire demarrer par la fin de sequence donc z pour notre exemple
+
+print(alphabet_inverse)
