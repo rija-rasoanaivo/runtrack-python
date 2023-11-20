21a63779971fa233af38f789d7bfce85c62d82f8
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-20 15:30:20 +0100

    ajout du job pour aller plus loin

---
+++ jour01/aller_plus_loin/aller_plus_loin.py
@@ -0,0 +1,6 @@
+chaine_contenant_e = input ("Entrez un mot : ")
+
+if 'e' in chaine_contenant_e:
+    print ("la chaine contient le caractère 'e'")
+else:
+    print ("la chaine ne contient pas le caractère 'e'")
