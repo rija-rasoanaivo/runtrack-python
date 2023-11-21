3f08c8592aca1fcf5e3f6fc2226446261eee8048
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-21 10:39:18 +0100

    ajout de la proposition 2

--- jour02/job03/main.py
+++ jour02/job03/main.py
@@ -1,3 +1,11 @@
+#proposition 1
+
 for i in range(101):
     if i != 26 and i != 37 and i != 88:
         print(i)
+
+
+#proposition 2
+# for i in range(101):
+    # for i not in [26, 37,88]:
+        #print(i)
