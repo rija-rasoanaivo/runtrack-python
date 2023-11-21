5bf7fe76fa5c8953862e194d4c403c4457861778
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-21 15:14:52 +0100

    Màj finale , job terminé

--- jour02/aller_plus_loin/main.py
+++ jour02/aller_plus_loin/main.py
@@ -2,16 +2,23 @@ a = int(input("Entrez la longueur a :"))
 b = int(input("Entrez la longueur b :"))
 c = int(input("Entrez la longueur c :"))
 
-if a == b == c :
-    print("Vous avez un triangle équilatéral")
-elif a == b or a == c or b == c:
-    if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
-        print("Vous avez un triangle isocèle rectangle")
-    else :
-        print("Vous avez un triangle isocèle")
+# verifie si les longueurs peuvent former un triangle
+if a + b > c and a + c > b and b + c > a :
+    print("ces longueurs peuvent faire un triangle")
 
-elif a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
-    print("Vous avez un triangle rectangle")
+    if a == b == c :
+        print("Vous avez un triangle équilatéral")
+    elif a == b or a == c or b == c:
+        if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+            print("Vous avez un triangle isocèle rectangle")
+        else :
+            print("Vous avez un triangle isocèle")
 
+    elif a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+        print("Vous avez un triangle rectangle")
+
+    else:
+        print("Vous avez unt triangle quelconque")
+        
 else:
-    print("Vous avez unt triangle quelconque")
+    print("ce n'est pas un triangle")
