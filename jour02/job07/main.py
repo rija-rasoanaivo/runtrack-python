d8b6baaf2934cbc111434645a7569572a7e0a99c
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-21 13:59:11 +0100

    Màj avec une possibilité 2

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -1,9 +1,16 @@
 chaine = "abcdefghijklmnopqrstuvwxyz" * 10
 
-i = 1
-
-while i <= len(chaine):
+for i in range(1, len(chaine) + 1, 2):
     print(chaine[:i])
-    i += 2
-    if i > len(chaine):
-        break
+
+
+#posiilité 2
+#chaine = "abcdefghijklmnopqrstuvwxyz" * 10
+
+#i = 1
+
+#while i <= len(chaine):
+    #print(chaine[:i])
+    #i += 2
+    #if i > len(chaine):
+        #break
