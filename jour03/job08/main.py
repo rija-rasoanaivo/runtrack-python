54f142740c5fa8e97fd0f769b3240f3bff6bc61d
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-22 11:51:48 +0100

    ajout du job08

---
+++ jour03/job08/main.py
@@ -0,0 +1,15 @@
+def produits(type, saison):
+    if type == "fruits" and saison == "hiver":
+        print("Orange, mandarine et kiwi")
+    elif type == "fruits" and saison == "ete":
+        print("Poire, fraise, cassis")
+    elif type == "legume" and saison == "hiver":
+        print("Carotte, topinambour, endive")
+    elif type == "legume" and saison == "ete":
+        print("Artichaut, aubergine, navet")
+    else:
+        print("Combinaison type/saison non reconnue")
+produits("fruits", "hiver")
+produits("fruits", "ete")
+produits("legume", "hiver")
+produits("legume", "ete")
