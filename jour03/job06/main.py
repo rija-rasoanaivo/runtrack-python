f738ff73ade380027ab72dce80e07278e03e44ac
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-22 11:26:35 +0100

    ajout du job06

---
+++ jour03/job06/main.py
@@ -0,0 +1,11 @@
+def plus_ou_moins(nombre):
+    if nombre>0:
+        print("positif")
+    elif nombre<0:
+        print("negatif")
+    else:
+        nombre == 0
+        print("Ce nombre est égal à 0")
+plus_ou_moins(5)
+plus_ou_moins(-5)
+plus_ou_moins(0)
