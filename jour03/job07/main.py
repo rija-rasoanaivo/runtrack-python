10b371b18163c700f899a33a98bf8069657fb6cf
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-22 11:41:20 +0100

    ajout du job07

--- jour03/job07/main.py
+++ jour03/job07/main.py
@@ -5,9 +5,11 @@ def les_languages(langage):
         print("tu es un développeur IA")
     elif langage == "java":
         print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un développeur mobile")
     else:
-        print("Poulet")
+        print("un jour, je serai le meilleur développeur...")
 les_languages("JavaScript")
 les_languages("python")
 les_languages("java")
-les_languages("coucou")
+les_languages("poulet")
