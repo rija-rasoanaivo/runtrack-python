e27eda1bca620fa1b2dfe6ae4c7d86d6c8f39953
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-23 18:54:00 +0100

    Màj du job 14

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,4 +1,4 @@
-def my_long_word(number, text):#la fonction prend deux paramètres "number(chiffre entier)" et "text"
+def my_long_word(number, text):#la fonction prend deux paramètres "number(chiffre entier)" et text
     words = text.split()#découpe de la chaîne text en une liste de mots en utilisant l'expace comme séparateur et stocke ces mots ces mots dans la variable "words"
     result = ""#Initialisation d'une chaine vide "result" qui stockera les mots plus longs que "number"
 
@@ -10,7 +10,7 @@ def my_long_word(number, text):#la fonction prend deux paramètres "number(chiff
         if word_length > number:#après avoir parcouru tous les caractères du mot, elle vérifie si "word_length" est supérieur à "number"
             result += word + " "#si la longueur du mot est supérieure au choffre donnée, elle ajoute ce mot à la chaîne "result" suivie d'un espace à l'aide de guillemets " "
 
-    return result()#la fonction retourne la chaîne de caractères "result"
+    return result#la fonction retourne la chaîne de caractères 
 
 output = my_long_word(3, "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance")
 print("Output:", output)
