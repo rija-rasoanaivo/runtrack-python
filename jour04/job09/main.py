d957b694038ecc090eb34fe872ce8bc5a56facfd
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-23 12:30:08 +0100

    ajout du job09

---
+++ jour04/job09/main.py
@@ -0,0 +1,12 @@
+L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+print(f"la valeur max est:",max(L))
+print(f"la valeur min est:",min(L))
+
+def max_min(L):
+    v_max=max(L)
+    v_min=min(L)
+    return (v_max, v_min)
+v_max=max(L)
+v_min=min(L)
+print(f"la valeur max est:{v_max}")
+print(f"la valeur max est:{v_min}")
