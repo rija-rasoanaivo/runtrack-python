64677c2671fc73a54bed8e7ec6e871670bfc2c73
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-23 14:48:56 +0100

    Màj

--- jour04/job06/main.py
+++ jour04/job06/main.py
@@ -1,10 +1,10 @@
-def echange_valeur():
-    valeur=list(range(1,6))
-    return valeur
-print(echange_valeur())
+valeur=list(range(1,6))
 
-def echange_valeur():
-    valeur=list(range(1,6))
-    valeur[0], valeur[-1] = valeur[-1], valeur[0]
-    return valeur
-print(echange_valeur())
+def echange_valeur(liste):
+    return liste
+print(echange_valeur(valeur))
+
+def echange_valeur(liste):
+    liste[0], liste[-1] = liste[-1], liste[0]
+    return liste
+print(echange_valeur(valeur))
