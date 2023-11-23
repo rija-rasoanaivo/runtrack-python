1d91c6c686ce652cb50e4143162689dff9962613
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-23 15:46:16 +0100

    Màj du job en ajoutant une autre possibilité

--- jour04/job11/main.py
+++ jour04/job11/main.py
@@ -3,4 +3,10 @@ def addition(liste):
     newlist=[i + 1 for i in liste]
     return newlist
 print(addition(L))
-    
+
+#autre possibilité mais bof je trouve car ça affiche un "none"    
+def list():
+    for i in range(len(L)):
+        L[i] += 1
+    print(L)
+print(list())
